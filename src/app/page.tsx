cea48b59b39c33c05f4dd342740a3589b2f84214
mukulpadwal <mukulpadwal.me@gmail.com>
2024-04-02 12:04:07 +0530

    ADD : hero-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <section className="flex min-h-screen flex-col items-center justify-between"></section>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+    </main>
   );
 }
